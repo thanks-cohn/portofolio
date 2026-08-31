26172ef8fed23aac562c38fe26cf2ea2ddbfba98
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-31 03:39:14 -0500

    Launch final site portfolio builder

--- tools/truth_csv_editor.py
+++ tools/truth_csv_editor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Compatibility entrypoint for the Quandranea visual portfolio builder."""
 
-from portfolio_builder_simple import main
+from portfolio_builder_site import main
 
 
 if __name__ == "__main__":
